7c096d63e2352b66bf3f8609463ebc518e24e466
Max <maxim.macari.dam@gmail.com>
2020-11-19 22:40:42 +0100

    Initial Commit

---
+++ HoneyComb_CustomGridView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HoneyComb_CustomGridView
+//
+//  Created by Maxim Macari on 19/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HoneyComb_CustomGridView/HoneyComb_CustomGridViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneyComb_CustomGridViewApp.swift
+//  HoneyComb_CustomGridView
+//
+//  Created by Maxim Macari on 19/11/20.
+//
+
+import SwiftUI
+
+@main
+struct HoneyComb_CustomGridViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
